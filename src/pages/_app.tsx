d9ac3b9277ba234efe569fffeaca5118f06e0b21
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2021-07-09 17:52:24 +0300

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "@style/globals.css";
+import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Google Docs Clone</title>
+        <meta
+          name="description"
+          content="Google Docs Clone created using React"
+        />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
